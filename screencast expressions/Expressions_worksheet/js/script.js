ad9d83504dfbb73deafc2ead1aaf853b31ef4761
Jeramie <jbrehm@fullsail.edu>
2013-04-17 02:17:17 -0400

    Initial file creation and completion of Dog years portion of worksheet.

---
+++ screencast expressions/Expressions_worksheet/js/script.js
@@ -0,0 +1,15 @@
+// Written by Jeramie Brehm, 17APR2013, Expressions Worksheet //
+
+//Dog Years
+//Calculate Sparky's dog year age based on actual age
+
+//define actual age
+var sparkyAgeHuman = 5;
+
+//multiply actual by dog year factor and assign variable
+var sparkyAgeDog = sparkyAgeHuman * 7;
+
+//display sparky's age in dog years
+console.log("Sparky is " + sparkyAgeHuman + ' human years old which is ' + sparkyAgeDog + " in dog years.");
+
+//Slice of Pie part 1
