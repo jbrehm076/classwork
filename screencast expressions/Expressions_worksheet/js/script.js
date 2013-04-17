c72bc4953f43957f0e5f0159f7809765fe19121c
Jeramie <jbrehm@fullsail.edu>
2013-04-17 02:31:30 -0400

    updated js for Slice of Pie part 1. note: static values inserted to test calculations.

--- screencast expressions/Expressions_worksheet/js/script.js
+++ screencast expressions/Expressions_worksheet/js/script.js
@@ -12,4 +12,23 @@ var sparkyAgeDog = sparkyAgeHuman * 7;
 //display sparky's age in dog years
 console.log("Sparky is " + sparkyAgeHuman + ' human years old which is ' + sparkyAgeDog + " in dog years.");
 
+
+
 //Slice of Pie part 1
+//calculate amound of pizza per person.
+
+//total slices of pizza
+var slicesPerPizza = 8;
+
+//total people at party
+var totalPeople = 4;
+
+//total pizzas ordered
+var totalPizza = 2;
+
+//mult. pizzas by number of slices, and div. by total people for slices per person.
+var slicesPerPerson = totalPizza * slicesPerPizza / totalPeople;
+
+//display results in format requirement
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
