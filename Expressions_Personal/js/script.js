351fb4a73e88581a6a8c52e5d70b9a7786717d9c
Jeramie <jbrehm@fullsail.edu>
2013-04-18 12:03:56 -0400

    Line 6 defined deviceType variable and added prompt.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,6 @@
 // JavaScript written by Jeramie Brehm 18APR2013 Expressions Personal
 
 //determine how storage is used on iphone and by songs and pictures.
+
+//define device
+var deviceType = prompt("What type of device are you using to store your songs and photos? i.e. ipod"); 
