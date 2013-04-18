e0dbd43378eabb5f6584445714da80816b35df16
Jeramie <jbrehm@fullsail.edu>
2013-04-18 12:57:49 -0400

    Line 24: added console dialog, with concat. statement.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -19,3 +19,6 @@ var productHist = [850356, 935103, 1000324, 950232];
 
 //define historical product move avg; array calc. add historical data, divide by 4 weeks for monthly avg product moves.
 var productHistAvg = (productHist[0] + productHist[1] + productHist[2] + productHist[3]) / 4;
+
+//display data to console in writer defined format.
+console.log("You're currently trending a monthly average of " + productMpm + " moves per month with a historical average of " + productHistAvg + " moves last month.");
