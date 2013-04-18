c318f6153730a6a77ab4e1125745dc021261121d
Jeramie <jbrehm@fullsail.edu>
2013-04-18 11:24:06 -0400

    Updated line 10 prompt to state data input to be in inches.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,7 +8,7 @@ var beastName = prompt("What's the name of your 4x4 rig?");
 var tireDiaOld = prompt("What is the current tire diameter in inches on your mall-crawler?");
 
 //define new tire size
-var tireDiaNew = prompt ("What diameter are the tires you want to put on that stump-jumper?");
+var tireDiaNew = prompt ("What diameter in inches are the tires you want to put on that stump-jumper?");
 
 //define mph to show differences
 var mphGiven = prompt("Enter speedometer reading in miles per hour(mph), you want to test?");
