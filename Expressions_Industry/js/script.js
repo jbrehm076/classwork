2e77a9273787e137a94834ebbb2c4225e6710b76
Jeramie <jbrehm@fullsail.edu>
2013-04-18 13:03:42 -0400

    Line 18 updated new array statement.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -14,6 +14,9 @@ var productNumber = prompt("how much product is in each container?");
 //define productMPH; calculation: divide 60 by delivery time for moves per hour. mult. by number of vehicles for total moves per hour. mult * product per container for total product moved per hour. mult by 672 for projected monthly avg at current numbers.
 var productMpm = 60 / transportTime * vehicleNumber * productNumber * 672;
 
+//Add new array
+var prodHist = new Array(4);
+
 //define 4 weeks historical product moved.
 var productHist = [850356, 935103, 1000324, 950232];
 
