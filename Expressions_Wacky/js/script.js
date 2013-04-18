733f6433bf9ad9a34fe6a5331c6d64888719df57
Jeramie <jbrehm@fullsail.edu>
2013-04-18 11:07:31 -0400

    Defined beastName  variable w/ prompt.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 // JavaScript written by Jeramie Brehm 17APR2013; Expressions Wacky //
 
 //Purpose Determine mph difference when using larger tires on your 4x4.
+//define the machine's name
+var beastName = prompt("What's the name of your 4x4 rig?");
