452e0100d42312562b0df9a48933962ba23ea1d7
Jeramie <jbrehm@fullsail.edu>
2013-04-18 12:47:28 -0400

    Line 6: defined variable vehicleNumber and added prompt.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,6 @@
 // JavaScript written by Jeramie Brehm 17APR2013; Expressions Industry //
 
 //Using interrogatives, calculate total product moves per hour of site automation vehicles.
+
+//define number of vehicles on track at site.
+var vehicleNumber = prompt("How many vehicles does your site have on the track?");
