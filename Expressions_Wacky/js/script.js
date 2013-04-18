a7f94f4ff6af8ddfb3f04aed5ff57a386f2e4aa4
Jeramie <jbrehm@fullsail.edu>
2013-04-18 11:25:26 -0400

    Updated line 14  sentence structure.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,7 +11,7 @@ var tireDiaOld = prompt("What is the current tire diameter in inches on your mal
 var tireDiaNew = prompt ("What diameter in inches are the tires you want to put on that stump-jumper?");
 
 //define mph to show differences
-var mphGiven = prompt("Enter speedometer reading in miles per hour(mph), you want to test?");
+var mphGiven = prompt("Enter what speedometer reading in miles per hour(mph), that you want to test?");
 
 //mult. new tire diameter by speedometer reading, divided by old tire diameter to determine actual speed.
 var speedActual = tireDiaNew * mphGiven / tireDiaOld;
