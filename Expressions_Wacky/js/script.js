cbd24f6dbe740364570692f1e44fe6e169d30d6b
Jeramie <jbrehm@fullsail.edu>
2013-04-18 11:23:18 -0400

    Updated line 8 to define variable input to be in inches.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,7 +5,7 @@
 var beastName = prompt("What's the name of your 4x4 rig?");
 
 //define current tire size
-var tireDiaOld = prompt("What is the current tire diameter on your mall-crawler?");
+var tireDiaOld = prompt("What is the current tire diameter in inches on your mall-crawler?");
 
 //define new tire size
 var tireDiaNew = prompt ("What diameter are the tires you want to put on that stump-jumper?");
