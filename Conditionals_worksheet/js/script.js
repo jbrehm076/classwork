16338234bcf53f30edcaf6f68c375d325d59fd38
Jeramie <jbrehm@fullsail.edu>
2013-04-23 07:59:31 -0400

    L13: fixed percentage issue

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,13 +4,13 @@
 //purpose: determine if refuel is needed in order to reach next station 200 miles away.
 
 //define variables and collect data
-var gasEff = prompt("What is the efficientcy of your vehicle in mpg?");
+var gasEff = prompt("What is the efficiency of your vehicle in mpg?");
 
 var gasRead = prompt("How full is your gas tank (in percent)?");
 var gasCap = prompt("How gallons of fuel does your fuel tank hold?");
 
 //determine amount of fuel= capacity times percentage read. det. max range with calculated amount of fuel = amount times mpg.
-var fuelNow = gasCap * gasRead
+var fuelNow = gasCap * (gasRead * .01)
 var carRange = fuelNow * gasEff;
 
 //if / then statement to determine if refueling needed with required else statement
