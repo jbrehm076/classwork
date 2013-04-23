088debc417e0f50da9ce420a6385057a8fba585e
Jeramie <jbrehm@fullsail.edu>
2013-04-23 08:02:08 -0400

    L7-9: resequenced variables for easier manual computation.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,9 +4,10 @@
 //purpose: determine if refuel is needed in order to reach next station 200 miles away.
 
 //define variables and collect data
+var gasRead = prompt("How full is your gas tank (in percent)?");
+
 var gasEff = prompt("What is the efficiency of your vehicle in mpg?");
 
-var gasRead = prompt("How full is your gas tank (in percent)?");
 var gasCap = prompt("How gallons of fuel does your fuel tank hold?");
 
 //determine amount of fuel= capacity times percentage read. det. max range with calculated amount of fuel = amount times mpg.
