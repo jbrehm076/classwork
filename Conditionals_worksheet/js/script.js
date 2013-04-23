b00e8a2efe7a0e3dee4b7a0104879e2b1f88fa60
Jeramie <jbrehm@fullsail.edu>
2013-04-23 07:31:34 -0400

    L17-23 Setup if else statement syntax; pre-variable insertion.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -12,3 +12,12 @@ var gasCap = prompt("How gallons of fuel does your fuel tank hold?");
 //determine amount of fuel= capacity times percentage read. det. max range with calculated amount of fuel = amount times mpg.
 var carRange = gasCap * gasRead * gasEff;
 
+//if / then statement to determine if refueling needed with required else statement
+
+if(){
+	//if range is less than 200 miles
+	console.log();
+}else{
+	//
+	console.log();
+}
