f912a56d174f3689e5448d26a0aa3034f16bf9b0
Jeramie <jbrehm@fullsail.edu>
2013-04-23 07:10:13 -0400

    L3-4: Decided on question and stated purpose.

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,4 @@
+// JavaScript  Jeramie Brehm 23 April 2013; Conditionals_worksheet 
+
+//Last Chance for Gas!
+//purpose: determine if refuel is needed in order to reach next station 200 miles away.
