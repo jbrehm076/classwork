17ceef62f0a5c300f460053b815e31e7d98a3041
Jeramie <jbrehm@fullsail.edu>
2013-04-25 09:36:20 -0400

    L5: added variable definition statement

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,5 @@
 // JavaScript written by Jeramie Brehm 24APR2013; Conditionals Wacky //
 
 //determine how much it will cost to buy dog food. Premium is $2 dollars a pound. Standard is $.75 dollars a pound. There is a ten percent discount for premium food when going in the morning.
+
+//define how many lbs of premium dog food and establish variable for use in discounting 10 percent.
