ead34c73beedb0738d6557752434fe085019d600
Jeramie <jbrehm@fullsail.edu>
2013-04-25 10:03:11 -0400

    added comment and definition of variable prompting for systolic pressure.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,6 @@
 // JavaScript written by Jeramie Brehm 24 April 2013. Conditionals Personal
 
 //determine if use of nitro medication can be used or contraindications exist. Systolic pressure must be above 100 mmHg. No use of erectile disfunction (ED) medications, number of doses have they taken.
+
+//define systolic variable and prompt for input
+var bpSys = prompt("What is current systolic pressure in mmHg? i.e. 120 / 80 where 120 is the systolic pressure.");
