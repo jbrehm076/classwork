f76ef8d2f3bfee8353b3791c4f7ca12900bd85ce
Jeramie <jbrehm@fullsail.edu>
2013-04-25 10:28:04 -0400

    coded tool number variable and prompted for data.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,3 +2,4 @@
 
 //determine if you can take a break and still get maintenance done. Each tool takes 20 min. Personal break is 1 hr. Determine if you can complete maint. and take a break.
 //define number of tools requiring maint.
+var toolNum = prompt("How many tools are scheduled for maintenance for this shift?");
