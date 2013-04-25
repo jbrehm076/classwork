6986f0341f3c9ebcc7f99aa48dfa8f6e88e75653
Jeramie <jbrehm@fullsail.edu>
2013-04-25 10:29:25 -0400

    commented to define variable for length of shift.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,3 +3,5 @@
 //determine if you can take a break and still get maintenance done. Each tool takes 20 min. Personal break is 1 hr. Determine if you can complete maint. and take a break.
 //define number of tools requiring maint.
 var toolNum = prompt("How many tools are scheduled for maintenance for this shift?");
+
+//define how long is shift.
