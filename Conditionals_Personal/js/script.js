725e0ffe680d72ffa6885a633eb7581be6f854e4
Jeramie <jbrehm@fullsail.edu>
2013-04-25 10:02:22 -0400

    added purpose statement concerning medical treatment.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,3 @@
 // JavaScript written by Jeramie Brehm 24 April 2013. Conditionals Personal
+
+//determine if use of nitro medication can be used or contraindications exist. Systolic pressure must be above 100 mmHg. No use of erectile disfunction (ED) medications, number of doses have they taken.
