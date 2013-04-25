a0dffd741e3410e600d07f6efaa111ea57a7cf78
Jeramie <jbrehm@fullsail.edu>
2013-04-25 10:26:31 -0400

    commented purpose of conditional.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,3 @@
 // JavaScript Coded by Jeramie Brehm 24APR2013; Conditionals Industry
+
+//determine if you can take a break and still get maintenance done. Each tool takes 20 min. Personal break is 1 hr. Determine if you can complete maint. and take a break.
