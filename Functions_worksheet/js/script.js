5d17b65d195e1eb7e840368b64b21952fc207d58
Jeramie <jbrehm@fullsail.edu>
2013-04-30 19:30:43 -0400

    Creating base file for functions worksheet

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,9 @@
+// Written by Jeramie Brehm, 30APR2013, Screencast: Functions //
+
+function dogYears(age){
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.")
+}
+var age1 = 4;
+dogYears(age1);
+dogYears(5);
