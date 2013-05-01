eab5bec1456c67d97e0c81bc29c1a6757295f239
Jeramie <jbrehm@fullsail.edu>
2013-04-30 20:05:09 -0400

    L18: added closing squigs

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -15,6 +15,7 @@ var circTot = calcCirc(circRad, pi);
 function calcCirc(circRad, pi){
 	var circumference = 2 * circRad * pi;
 	return circumference;
+}
 //display to console text message with completed circumference.
 console.log("The circumference of the circle is " + circTot;
 	//
