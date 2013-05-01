46845cac2311f7d7f674d77edf5c10ccfdab5156
Jeramie <jbrehm@fullsail.edu>
2013-04-30 20:55:04 -0400

    Created folders and filestructure for Functions assignment.

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,39 @@
+// JavaScript Coded by Jeramie Brehm 24APR2013; Conditionals Industry
+
+//determine if you can take a break and still get maintenance done. Each tool takes 20 min. Personal break is 1 hr. Determine if you can complete maint. and take a break.
+//define number of tools requiring maint.
+var toolNum = prompt("How many tools are scheduled for maintenance for this shift?");
+
+//define how long is shift.
+var shiftTime = prompt("How many hours are alloted for maintenance for this shift?");
+
+//Validation
+//Check for null or lack of data collected from prompts to user using or boolean statement.
+if(toolNum == "" || shiftTime == ""){
+		   //undefined variable found. display to console verbally admonishing user.
+		   console.log("If you fail to completely fill in the blanks, I cannot answer you accurately.");
+//else operand		   
+}else{
+	  //validation complete without errors found, display to console data correctly submitted.
+	  console.log("Data submitted. Please wait.");
+}
+
+//calculate total maintenance time. number of tools times 20 = total time in min, then divide by 60 for hours. define maint. time variable.
+var maintTime = toolNum * 20 / 60;
+
+// define breakTime. total maint. time plus 1 hr for a break.
+var breakTime = maintTime + 1; 
+
+//compare total maintenance time to total shift 
+if(maintTime > shiftTime){
+	//not enough time to complete maintenance
+	console.log("Total maintenance time required exceeds the length of shift.");
+//compare shift time to total time with break included.
+}else if(breakTime > shiftTime){
+	//break time won't fit into schedule
+	console.log("Sorry but you won't be able to take a break this shift.");
+}else{
+	//there is enough time to take a break within the shift and complete maint.
+	console.log("You have enough time to take a break this shift and complete your maintenance.");
+}
+
---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,40 @@
+// JavaScript written by Jeramie Brehm 24 April 2013. Conditionals Personal //
+
+//determine if use of nitro medication can be used or contraindications exist. Systolic pressure must be above 100 mmHg. No use of erectile disfunction (ED) medications, number of doses have they taken.
+//define systolic variable and prompt for input
+var bpSys = prompt("What is current systolic pressure in mmHg? i.e. 120 / 80 where 120 is the systolic pressure.");
+
+//define ED variable and prompt for data. Define nitro eligibility variable for later use 
+var erectDisf = prompt("Are you currently on erectile disfunction (ED) medications? Yes or No.");
+var edYesNo
+//define dosage variable for how many doses already taken. 
+var nitroTaken = prompt("How many doses of nitro have you taken already?");
+
+//Validation
+// Validate whether any of the fields prompted to user contain data. Using OR boolean equation in if statement.
+if(bpSys == "" || erectDisf == "" || nitroTaken == ""){
+	//display that user did not correctly input data requested.
+	console.log("You did not correctly supply data needed.");
+}else{
+	//display to console data entry sufficient.
+	console.log("Data submitted. Please Wait.");
+}
+
+//determine total doses of nitro remaining. Three minus nitroTaken variable. Define nitro available var.
+var nitroAvail = 3 - nitroTaken;
+
+//If ED med taken, patient not able to take nitro pre-hospital. Ternary with statement to console.
+edYesNo = (erectDisf == "Yes"  || erectDisf == "yes") ? "Due to medications already taken, you cannot take nitro" : "No ED contraindications present.";
+
+//display ternary result variable to console.
+console.log(edYesNo);
+
+//determine if systolic pressure is at acceptable levels and how many doses can be administered. Incorporate all variables in AND boolean conditional.
+if((erectDisf == "No"  || erectDisf == "no") && bpSys > 100 && nitroAvail > 0){
+	//display to console instructional message for true results of if statement.
+	console.log("With a systolic of " + bpSys + " mmHg, you are eligible for nitro treatment. You have " + nitroAvail + " dose(s) available. Administer one now with remaining dose(s) to be taken at 5 min intervals if chest pain does not subside. Call 911.");
+}else{
+	//display to console instructional message for false results of if boolean statement.
+	console.log("It is not safe to take nitro at this time. Call 911 immediately."); 
+
+}
