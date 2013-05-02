c1aa12f543f8ef6ed5c23aee078cf9c9f55e1b92
jbrehm <jbrehm@fullsail.edu>
2013-05-02 09:39:02 -0400

    added function section of code

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,5 @@
 
 //area supervisors have been promoted and there is now vacancies. determine if you are eligible.
 //Requirements are 3 years in current position. How many are eligible. What is your percentage if everyone is equally qualified.
+
+//FUNCTION
