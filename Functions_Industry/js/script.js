c638677338cdf40345bbf8d4756e325b1110b703
jbrehm <jbrehm@fullsail.edu>
2013-05-02 09:36:31 -0400

    updated js header info

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,4 +1,4 @@
-// JavaScript Coded by Jeramie Brehm 24APR2013; Conditionals Industry
+// JavaScript Coded by Jeramie Brehm 01MAY2013; Functions Industry
 
 //determine if you can take a break and still get maintenance done. Each tool takes 20 min. Personal break is 1 hr. Determine if you can complete maint. and take a break.
 //define number of tools requiring maint.
