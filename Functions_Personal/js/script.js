02d179057a4467c40864b6e22f8d49836cb30fcd
jbrehm <jbrehm@fullsail.edu>
2013-05-02 09:21:55 -0400

    defined curTime variable with prompt

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -14,3 +14,5 @@ var calcTimePer = function(lastTime, percent){
 //VARIABLES
 //define previous time variable with prompt for data
 var prevTime = prompt("What was your last time for 5 mile run in minutes? i.e. 5, 4.5");
+//define variable for current time with prompt
+var curTime = prompt("What was your current time for 5 mile run in minutes? i.e. 5, 4.5");
