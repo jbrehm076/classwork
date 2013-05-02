a60170eba1abb70acb7515d9cb89833ec59f21f5
jbrehm <jbrehm@fullsail.edu>
2013-05-02 08:58:40 -0400

    defined bgCheck varaible and prompt

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,6 +2,7 @@
 
 //Free AK-47 with truck purchase. As advertised in Florida, a truck dealership gave vouchers for free AK-47 rifles with purchase of a new truck. It did not guarantee but offer payment for those qualified to obtain and own an AK-47. For this work, I will be assuming qualifications to include, 18 yrs of age, qualifying background check, and voucher for the weapon from truck dealership.
 
+
 //variables
 //define birth year month and prompt for data
 var birthM = prompt("What month were you born in? Use number for month. i.e. October = 10");
@@ -11,3 +12,5 @@ var birthY = prompt("What year were you born in? Please use four digit year. i.e
 var currentM = prompt("What month are we currently in? Use number for month. i.e. April = 4");
 //define current year and prompt for data.
 var currentY = prompt("What year are we currently in? Please use four digit year. i.e. 1947");
+//define background check variable w/ prompt.
+var bgCheck = prompt("Did you pass your background check? yes or no");
