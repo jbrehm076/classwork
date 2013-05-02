7d6bd2b37468845d56cd422007002eafa01fbe19
jbrehm <jbrehm@fullsail.edu>
2013-05-02 09:38:34 -0400

    comment for requirements for code.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,4 @@
 // JavaScript Coded by Jeramie Brehm 01MAY2013; Functions Industry
 
 //area supervisors have been promoted and there is now vacancies. determine if you are eligible.
+//Requirements are 3 years in current position. How many are eligible. What is your percentage if everyone is equally qualified.
