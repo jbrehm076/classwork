5f9e48a2ebbb348e552f2802b67c9c34cd2cf82e
jbrehm <jbrehm@fullsail.edu>
2013-05-02 08:53:07 -0400

    added variables section

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 // JavaScript written by Jeramie Brehm 1MAY2013; Professional Wacky //
 
 //Free AK-47 with truck purchase. As advertised in Florida, a truck dealership gave vouchers for free AK-47 rifles with purchase of a new truck. It did not guarantee but offer payment for those qualified to obtain and own an AK-47. For this work, I will be assuming qualifications to include, 18 yrs of age, qualifying background check, and voucher for the weapon from truck dealership.
+
+//variables
