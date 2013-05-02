f828799852fa3d808fbc5d7109ada254dacb9874
jbrehm <jbrehm@fullsail.edu>
2013-05-02 08:54:49 -0400

    added define comment and birthM prompt

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,5 @@
 //Free AK-47 with truck purchase. As advertised in Florida, a truck dealership gave vouchers for free AK-47 rifles with purchase of a new truck. It did not guarantee but offer payment for those qualified to obtain and own an AK-47. For this work, I will be assuming qualifications to include, 18 yrs of age, qualifying background check, and voucher for the weapon from truck dealership.
 
 //variables
+//define birth year month and prompt for data
+var birthM = prompt("What month were you born in? Use number for month. i.e. October = 10");
